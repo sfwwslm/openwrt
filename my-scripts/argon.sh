995f5dbe5562f163402b8fa1bd3f27e6cbe023fe
8845hs-22-wsl <aw4@qq.com>
2025-04-17 18:05:50 +0800

    更新脚本
    Signed-off-by: 8845hs-22-wsl <aw4@qq.com>

---
+++ my-scripts/argon.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
+rm -rf package/feeds/luci/luci-theme-argon
+mv luci-theme-argon package/feeds/luci/luci-theme-argon
