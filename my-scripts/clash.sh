9369ac407e876c7182469f7830b7b4c6c1743ed5
main-wsl <sfwwslm@gmail.com>
2025-04-20 00:10:47 +0800

    修改内核平台下载错误的问题
    Signed-off-by: main-wsl <sfwwslm@gmail.com>

--- my-scripts/clash.sh
+++ my-scripts/clash.sh
@@ -6,23 +6,23 @@ rm -rf package/feeds/luci/luci-app-openclash
 mv OpenClash/luci-app-openclash package/feeds/luci/luci-app-openclash
 rm -rf OpenClash
 
-##---------------- dev core ----------------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p package/feeds/luci/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##---------------------------------------------------------
+# ##---------------- dev core ----------------------------
+# curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+# tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+# chmod +x /tmp/clash >/dev/null 2>&1
+# mkdir -p package/feeds/luci/luci-app-openclash/root/etc/openclash/core
+# mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+# rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+# ##---------------------------------------------------------
 
-##------------- tun core --------------------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz -o /tmp/clash.gz
-gzip -d /tmp/clash.gz /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
+# ##------------- tun core --------------------------------
+# curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17-13-gdcc8d87.gz -o /tmp/clash.gz
+# gzip -d /tmp/clash.gz /tmp >/dev/null 2>&1
+# chmod +x /tmp/clash >/dev/null 2>&1
+# mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
 
 ##------------- meta core ---------------------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
 mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
