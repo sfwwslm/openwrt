3776178c3356ab9ca3f3d8900127fe27b3c99290
main-wsl <sfwwslm@gmail.com>
2025-04-20 01:34:26 +0800

    修复缺陷目录的问题
    Signed-off-by: main-wsl <sfwwslm@gmail.com>

--- my-scripts/clash.sh
+++ my-scripts/clash.sh
@@ -25,6 +25,7 @@ rm -rf OpenClash
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p package/feeds/luci/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash package/feeds/luci/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 
